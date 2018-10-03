87fbb8e8bbc12b8515f73ccaea69460ea057c1e1
Capril1042 <Caprilewis1042@gmail.com>
2018-10-02 22:24:11 -0400

    Adds Header

---
+++ calcalc/src/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header=(props) => {
+        return (
+            <div className="header">
+                Calorie Calculator
+            </div>
+        )
+}
+
+export default Header;
